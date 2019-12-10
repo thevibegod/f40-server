c3578d8b2d53a1eb9eeb87c294c65e582de133d3
Harikumar <harikumarkannanarul@gmail.com>
2019-12-10 11:18:36 +0530

    Changes

--- models/notificationschema.js
+++ models/notificationschema.js
@@ -1,2 +1,16 @@
 const mongoose = require('mongoose');
- Schema=mongoose.schema
+ Schema=mongoose.Schema;
+var notificationSchema=new mongoose.UserSchema
+{
+  title :{
+    type:String,
+    required:true,
+    unique:true
+  },
+  desc :
+  {
+    type:String,
+    required:true,
+  },
+}
+ module.exports = mongoose.model('Notification', notificationSchema);
