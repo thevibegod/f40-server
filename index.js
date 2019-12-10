929388a358ab72160b2da8949c563a9e73da8420
S Praveen Kumar <praveens.bec@gmail.com>
2019-12-10 12:21:06 +0530

    Added JWT auth

--- index.js
+++ index.js
@@ -1,45 +1,57 @@
-var express = require('express');
-var cors= require('cors')
-var bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const profileRouter = require("./routes/profiles");
+const userRouter = require("./routes/user");
+const auth = require("./auth/auth");
+const app = express();
 
-var profileRouter = require('./routes/profiles')
-var userRouter = require('./routes/user')
-var app = express();
-
-
-
-
-
-
-
-app.use((req,res,next)=>{
-  res.header("Access-Control-Allow-Origin","*");
-  res.header("Access-Control-Allow-Headers","*");
-  next();
-})
-
-
-const connectDB = require('./config/db');
-app.use(bodyParser.json())
+const connectDB = require("./config/db");
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 connectDB();
-app.set('view engine','ejs')
+app.set("view engine", "ejs");
 
-app.use(profileRouter)
-app.use(userRouter)
-app.get('/', function(req, res){
-   res.send("GET Login/Home page");
+app.post("/validateuser", (req, res) => {
+  // fetch user and test password verification
+  require("./models/UserSchema").findOne(
+    { username: req.body.username },
+    function(err, user) {
+      if (err) throw err;
+      user.comparePassword(req.body.password, function(err, isMatch) {
+        if (err) throw err;
+        else {
+          if (isMatch) {
+            res.json({
+              success: true,
+              msg: "Login Successful",
+              token: auth.generateToken(user.username)
+            });
+          } else {
+            res.json({
+              success: false,
+              msg: "Invalid username or password"
+            });
+          }
+        }
+      });
+    }
+  );
 });
 
-app.post('/', function(req, res){
-   res.send("POST Login/Home page");
+app.use(auth.validateMiddleware);
+app.use(userRouter);
+app.use(profileRouter);
+app.get("/", function(req, res) {
+  res.send("GET Login/Home page");
 });
 
-
-app.get('*', function(req, res){
-   res.send('Error 404.Page not found');
+app.post("/", function(req, res) {
+  res.send("POST Login/Home page");
 });
 
+app.get("*", function(req, res) {
+  res.send("Error 404.Page not found");
+});
 
 app.listen(3000);
--- routes/profiles.js
+++ routes/profiles.js
@@ -1,34 +1,37 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-var mongoose = require('mongoose')
-var profileSchema = require('../models/profileSchema')
-var multer=require('multer');
-var crypto = require('crypto');
-var path = require('path');
-var GridFsStorage = require('multer-gridfs-storage');
-var Grid = require('gridfs-stream');
+var mongoose = require("mongoose");
+var profileSchema = require("../models/profileSchema");
+var multer = require("multer");
+var crypto = require("crypto");
+var path = require("path");
+var GridFsStorage = require("multer-gridfs-storage");
+var Grid = require("gridfs-stream");
 
-const conn = mongoose.createConnection("mongodb+srv://adarsh18bec095:Adarsh123@f40cluster-wugpz.mongodb.net/test?retryWrites=true&w=majority")
+const conn = mongoose.createConnection(
+  "mongodb+srv://adarsh18bec095:Adarsh123@f40cluster-wugpz.mongodb.net/test?retryWrites=true&w=majority"
+);
 let gfs;
 
-conn.once('open',function(){
-  gfs=Grid(conn.db,mongoose.mongo);
-  gfs.collection('uploads')
-})
+conn.once("open", function() {
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection("uploads");
+});
 
 var storage = new GridFsStorage({
-  url: "mongodb+srv://adarsh18bec095:Adarsh123@f40cluster-wugpz.mongodb.net/test?retryWrites=true&w=majority",
+  url:
+    "mongodb+srv://adarsh18bec095:Adarsh123@f40cluster-wugpz.mongodb.net/test?retryWrites=true&w=majority",
   file: (req, file) => {
     return new Promise((resolve, reject) => {
       crypto.randomBytes(16, (err, buf) => {
         if (err) {
           return reject(err);
         }
-        const filename = buf.toString('hex') + path.extname(file.originalname);
+        const filename = buf.toString("hex") + path.extname(file.originalname);
         const fileInfo = {
           filename: filename,
-          bucketName: 'uploads'
+          bucketName: "uploads"
         };
         resolve(fileInfo);
       });
@@ -37,57 +40,92 @@ var storage = new GridFsStorage({
 });
 const upload = multer({ storage });
 
-
-router.get('/addprofile',function(req, res){
-  res.render('profile')
+router.get("/addprofile", function(req, res) {
+  res.render("profile");
 });
-router.get('/updateprofile',function(req, res){
-  res.render('profile2')
-});
-
-router.post('/addprofile',upload.single('picture'),(req, res)=>{
-  const newProfile = new profileSchema({name:req.body.name,id:req.file.id,batch:req.body.batch,attendance:req.body.attendance,mentorName:req.body.mentorName,rollNo:req.body.rollNo})
-  newProfile.save().then(()=>res.status(201).send('Added Profile to db')).catch((err)=>res.json(err))
+router.get("/updateprofile", function(req, res) {
+  res.render("profile2");
 });
 
-router.get('/studentprofilepicture',(req,res)=>{
-  if(!req.query.rollNo){
-    return res.status(400).send("Bad Request.")
+router.post("/addprofile", upload.single("picture"), (req, res) => {
+  const newProfile = new profileSchema({
+    name: req.body.name,
+    id: req.file.id,
+    batch: req.body.batch,
+    attendance: req.body.attendance,
+    mentorName: req.body.mentorName,
+    rollNo: req.body.rollNo
+  });
+  newProfile
+    .save()
+    .then(() => res.status(201).send("Added Profile to db"))
+    .catch(err => res.json(err));
+});
+
+router.get("/studentprofilepicture", (req, res) => {
+  if (!req.query.rollNo) {
+    return res.status(400).send("Bad Request.");
   }
-  profileSchema.findOne({rollNo:req.query.rollNo}).then(data=>{
-    gfs.files.findOne({_id:data.id},(err,file)=>{
+  if (req.query.rollNo !== req.loggedInUser.username) {
+    return res.status(403).send("Invalid access");
+  }
+  profileSchema.findOne({ rollNo: req.query.rollNo }).then(data => {
+    gfs.files.findOne({ _id: data.id }, (err, file) => {
       const readStream = gfs.createReadStream(file.filename);
       readStream.pipe(res);
-    })
-  })})
+    });
+  });
+});
 
-  router.get('/studentprofiledetails',(req,res)=>{
-    if(!req.query.rollNo){
-      return res.status(400).send("Bad Request.")
+router.get("/studentprofiledetails", (req, res) => {
+  if (!req.query.rollNo) {
+    return res.status(400).send("Bad Request.");
+  }
+  if (req.query.rollNo !== req.loggedInUser.username) {
+    return res.status(403).send("Invalid access");
+  }
+  profileSchema.findOne({ rollNo: req.query.rollNo }).then(data => {
+    return res.status(200).json(data);
+  });
+});
+
+router.get("/removestudentprofiledetails", (req, res) => {
+  if (!req.query.rollNo) {
+    return res.status(400).send("Bad Request.");
+  }
+  profileSchema
+    .findOneAndDelete({ rollNo: req.query.rollNo })
+    .then(data => {
+      gfs.remove({ _id: data.id, root: "uploads" });
+    })
+    .then(res.status(200).send("Profile removed from db"));
+});
+
+router.post(
+  "/studentprofiledetailsupdation",
+  upload.single("picture"),
+  (req, res) => {
+    if (!req.body.rollNo) {
+      return res.status(400).send("Bad Request.");
     }
-    profileSchema.findOne({rollNo:req.query.rollNo}).then(data=>{
-        return res.status(200).json(data)
+    profileSchema
+      .findOneAndDelete({ rollNo: req.body.rollNo })
+      .then(data => {
+        gfs.remove({ _id: data.id, root: "uploads" });
       })
-    })
-
-    router.get('/removestudentprofiledetails',(req,res)=>{
-      if(!req.query.rollNo){
-        return res.status(400).send("Bad Request.")
-      }
-      profileSchema.findOneAndDelete({rollNo:req.query.rollNo}).then(data=>{
-        gfs.remove({_id:data.id,root:'uploads'})
-            }).then(res.status(200).send("Profile removed from db"))
+      .then(() => {
+        const newProfile = new profileSchema({
+          name: req.body.name,
+          batch: req.body.batch,
+          id: req.file.id,
+          attendance: req.body.attendance,
+          mentorName: req.body.mentorName,
+          rollNo: req.body.rollNo
+        });
+        newProfile.save();
       })
-
-
-
-    router.post('/studentprofiledetailsupdation',upload.single('picture'),(req,res)=>{
-      if(!req.body.rollNo){
-        return res.status(400).send("Bad Request.")
-      }
-      profileSchema.findOneAndDelete({rollNo:req.body.rollNo}).then(data=>{
-        gfs.remove({_id:data.id,root:'uploads'});}).then(()=>{
-          const newProfile = new profileSchema({name:req.body.name,batch:req.body.batch,id:req.file.id,attendance:req.body.attendance,mentorName:req.body.mentorName,rollNo:req.body.rollNo})
-          newProfile.save()}).then(()=>res.status(201).send('Updated Profile in db')).catch((err)=>res.json(err))
-    })
-module.exports=router
+      .then(() => res.status(201).send("Updated Profile in db"))
+      .catch(err => res.json(err));
+  }
+);
+module.exports = router;
