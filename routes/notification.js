dba668ceceeb51249a5fafa5a14da1cf6fdfc4b7
Harikumar <harikumarkannanarul@gmail.com>
2019-12-10 12:37:22 +0530

    notification

--- routes/notification.js
+++ routes/notification.js
@@ -1,6 +1,8 @@
-var  notificationSchema = require('../models/notificationSchema');
+var express = require('express');
+var router = express.Router();
+
 var mongoose = require('mongoose')
-var router = require('express').Router();
+var notificationSchema = require('../models/notificationSchema')
 
 var connStr = "mongodb+srv://adarsh18bec095:Adarsh123@f40cluster-wugpz.mongodb.net/test?retryWrites=true&w=majority";
 mongoose.connect(connStr, function(err) {
@@ -8,42 +10,47 @@ mongoose.connect(connStr, function(err) {
     console.log('Successfully connected to MongoDB');
 });
 
-router.get('/notificationuser',(req,res)=>{
-  res.render('user')
-})
+router.get('/addnotification',function(req, res){
+  res.render('notification1')
+});
+router.get('/updatenotification',function(req, res){
+  res.render('notification2')
+});
 
-router.post('/createuser',(req,res)=>{
-  const testUser = new UserSchema({username:req.body.username,password:req.body.password});
+router.post('/addnotification',(req, res)=>{
+  const newnotification = new eventnotification({title:req.body.title,desc:req.body.desc})
+  newEvent.save().then(()=>res.status(201).send('Added Event to db')).catch((err)=>res.json(err))
+});
 
-  // save user to database
-  testUser.save().then(res.status(201).send("User Created")).catch(err=>res.json(err));
-})
 
-router.post('/validateuser',(req,res)=>{
-  // fetch user and test password verification
-  UserSchema.findOne({ username: req.body.username }, function(err, user) {
-     if (err) throw err;
-      user.comparePassword(req.body.password, function(err, isMatch) {
-          if (err) throw err;
-          return res.send(isMatch)
-      });
-  });
-})
 
-router.delete('/user',(req,res)=>{
-  if(!req.query.username){
-    res.status(400).send("Bad Request")
-  }
-  UserSchema.findOneAndDelete({username:req.query.username}).then(()=>  res.status(200).send("User deleted."))
-})
+  router.get('/notificationdetails',(req,res)=>{
+    if(!req.query.title){
+      return res.status(400).send("Bad Request.")
+    }
+    notificationSchema.findOne({title:req.query.title}).then(data=>{
+        return res.status(200).json(data)
+      })
+    })
 
-router.put('/user',(req,res)=>{
-  if(!req.query.username){
-    res.status(400).send("Bad Request")
-  }
-  UserSchema.findOneAndDelete({username:req.query.username}).then(()=>{
-  const testUser = new UserSchema({username:req.body.username,password:req.body.password});
-  testUser.save().then(res.status(201).send("User Updated"));})
-})
+    router.get('/removenotification',(req,res)=>{
+      if(!req.query.title){
+        return res.status(400).send("Bad Request.")
+      }
+      eventnotification.findOneAndDelete({title:req.query.title}).then(data=>{
+        gfs.remove({_id:data.id,root:'uploads'})
+      }).then(res.status(200).send("notification removed from db"))
+      })
 
-module.exports = router;
+
+
+    router.post('/notificationupdation',(req,res)=>{
+      if(!req.body.rollNo){
+        return res.status(400).send("Bad Request.")
+      }
+      notificationSchema.findOneAndDelete({title:req.body.title}).then(data=>{
+        gfs.remove({_id:data.id,root:'uploads'});}).then(()=>{
+          const newEvent = new notificationSchema({title:req.body.title,id:req.file.id,desc:req.body.desc})
+          newEvent.save()}).then(()=>res.status(201).send('Updated notification in db')).catch((err)=>res.json(err))
+    })
+module.exports=router;
