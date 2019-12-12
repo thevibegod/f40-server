c792bf6ee565f7abc08376ba3a6db7c626ae7378
S Praveen Kumar <praveens.bec@gmail.com>
2019-12-12 11:26:19 +0530

    Fix issues in achievements calls

--- models/achievementsSchema.js
+++ models/achievementsSchema.js
@@ -1,12 +1,12 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 var ObjectId = mongoose.Schema.ObjectId;
 
 var achievementsSchema = new mongoose.Schema({
-  achievements:{type:String,required:true},
-  date : {
+  achievements: { type: String, required: true },
+  date: {
     type: String,
     required: true
   }
-})
+});
 
-module.exports = mongoose.model('achievements',achievementsSchema)
+module.exports = mongoose.model("achievements", achievementsSchema);
