af6b74d12dde1f3f6ee09d8858378a2cc336e71b
Adarsh <adarsh@thevibegod.com>
2019-12-14 20:49:48 +0530

    Completed Attendance Route

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Expose-Headers', 'Access-Control-*, Origin, X-Requested-With, Content-Type, Accept, Authorization');
- res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3333');
+ res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
  res.setHeader('Access-Control-Allow-Methods', 'HEAD, GET, POST, OPTIONS, PUT, PATCH, DELETE');
  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-*, Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', true);
--- routes/attendance.js
+++ routes/attendance.js
@@ -10,52 +10,32 @@ mongoose.connect(connStr, function(err) {
     console.log('Successfully connected to MongoDB');
 });
 
-// router.get('/addcourse',function(req, res){
-//   res.render('course1')
-// });
-// router.get('/updatecourse',function(req, res){
-//   res.render('course2')
-// });
-//
-// router.post('/addcourse',(req, res)=>{
-//   const newcourse = new courseSchema({title:req.body.title,desc:req.body.desc,
-//   price:req.body.price,financialaid:req.body.financialaid,link:req.body.link})
-//   newcourse.save().then(()=>res.status(201).send('Added course to db')).catch((err)=>res.json(err))
-// });
 
+router.get('/studentattendance',(req,res)=>{
+  if(!req.query.rollNo){
+    return res.status(400).send("Bad Request.")
+  }
+  attendanceSchema.findOne({rollNo:req.query.rollNo}).then(data=>{
+    return res.status(200).json(data)
+  })
+})
 
+router.post('/postattendance',(req,res)=>{
+  let arr=Object.keys(req.body);
+  arr.map((data,index)=>{
+    if(index!==0){
+      attendanceSchema.findOneAndDelete({rollNo:data}).
+      then(d=>{
+        let obj={date:req.body.date,value:req.body[data]};
+        let arr2=d.dates;
+        arr2.push(obj);
+        new attendanceSchema({rollNo:d.rollNo,dates:arr2}).save();
+      }).then(()=>res.status(201).json('Attendance Updated')).catch(err=>res.json(err))
 
-  router.get('/studentattendance',(req,res)=>{
-    if(!req.query.rollNo){
-      return res.status(400).send("Bad Request.")
     }
-    attendanceSchema.findOne({rollNo:req.query.rollNo}).then(data=>{
-        return res.status(200).json(data)
-      })
-    })
+  })
 
-    // router.get('/removecourse',(req,res)=>{
-    //   if(!req.query.title){
-    //     return res.status(400).send("Bad Request.")
-    //   }
-    //   courseSchema.findOneAndDelete({title:req.query.title}).then(res.status(200).send("course removed from db"))
-    //   })
-    //
-    //
-    //
-    // router.post('/courseupdation',(req,res)=>{
-    //   if(!req.body.title){
-    //     return res.status(400).send("Bad Request.")
-    //   }
-    //   courseSchema.findOneAndDelete({title:req.body.title}).then(()=>{
-    //       const newcourse = new courseSchema({title:req.body.title,desc:req.body.desc,
-    //       price:req.body.price,financialaid:req.body.financialaid,link:req.body.link})
-    //       newcourse.save()}).then(()=>res.status(201).send('Updated course in db')).catch((err)=>res.json(err))
-    // })
-    //
-    // router.get('/getallcourses',(req,res)=>{
-    //   courseSchema.find({}).then(function (course){
-    //     res.json(course);
-    //   })
-    // })
+})
+
+
 module.exports=router;
