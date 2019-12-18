08eab6f1293005507f2ba7ea862555f96c0f269d
Adarsh <adarsh@thevibegod.com>
2019-12-18 12:55:46 +0530

    task page completed

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.options('*', cors())
 app.use(cors());
-
+app.use(express.static('public'));
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Expose-Headers', 'Access-Control-*, Origin, X-Requested-With, Content-Type, Accept, Authorization');
  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
--- routes/tasks.js
+++ routes/tasks.js
@@ -2,12 +2,6 @@ const taskSchema = require('../models/taskSchema');
 const scoreSchema = require('../models/scoreSchema')
 var mongoose = require("mongoose");
 var router = require("express").Router();
-var multer=require('multer');
-var crypto = require('crypto');
-var path = require('path');
-var GridFsStorage = require('multer-gridfs-storage');
-var Grid = require('gridfs-stream');
-var bson=require('bson');
 var formidable = require('formidable');
 var fs = require('fs');
 
@@ -18,33 +12,13 @@ var createTimeStamp = ()=>{
   return date + ' ' + time;
 }
 
-const conn = mongoose.createConnection("mongodb+srv://adarsh18bec095:Adarsh123@f40cluster-wugpz.mongodb.net/test?retryWrites=true&w=majority")
-let gfs;
-
-conn.once('open',function(){
-  gfs=Grid(conn.db,mongoose.mongo);
-  gfs.collection('uploads')
-})
-
-var storage = new GridFsStorage({
-  url: "mongodb+srv://adarsh18bec095:Adarsh123@f40cluster-wugpz.mongodb.net/test?retryWrites=true&w=majority",
-  file: (req, file) => {
-    return new Promise((resolve, reject) => {
-      crypto.randomBytes(16, (err, buf) => {
-        if (err) {
-          return reject(err);
-        }
-        const filename = buf.toString('hex') + path.extname(file.originalname);
-        const fileInfo = {
-          filename: filename,
-          bucketName: 'uploads'
-        };
-        resolve(fileInfo);
-      });
-    });
-  }
+var connStr =
+  "mongodb+srv://adarsh18bec095:Adarsh123@f40cluster-wugpz.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(connStr, function(err) {
+  if (err) throw err;
+  console.log("Successfully connected to MongoDB");
 });
-const upload = multer({ storage });
+
 
 router.post('/addtask',(req,res)=>{
   if(!req.body.topic || !req.body.taskType){
@@ -75,12 +49,12 @@ router.post('/uploadtask',(req,res)=>{
     form.parse(req, function (err, fields, files) {
       var oldpath = files.attachment.path;
       var ext = files.attachment.name.split('.')[1];
-      var newpath = './tasks/' + req.query.rollNo + 'ml' + Date.parse(new Date()) + '.' + ext ;
+      var newpath = './public/tasks/' + req.query.rollNo + 'ml' + Date.parse(new Date()) + '.' + ext ;
       fs.rename(oldpath, newpath, function (err) {
         if (err) throw err;
         taskSchema.findOneAndDelete({topic:fields.topic}).then((data)=>{
           let attachmentArray = data.attachments;
-          let obj = {rollNo:req.query.rollNo,feedback:fields.feedback,attachmentId:newpath,timeStamp:createTimeStamp()};
+          let obj = {rollNo:req.query.rollNo,feedback:fields.feedback,attachmentId:newpath.slice(8),timeStamp:createTimeStamp()};
           attachmentArray.push(obj);
           new taskSchema({_id:data._id,taskType:fields.taskType,topic:fields.topic,attachments:attachmentArray,uploadTime:data.uploadTime,deadline:data.deadline}).save().then(()=>res.status(200).send("Task submitted."))
           .catch(err=>res.status(500).json(err));
@@ -90,39 +64,38 @@ router.post('/uploadtask',(req,res)=>{
     }
 
     else{
-      //this is for deleting a file in folder
-      // fs.unlink(attachmentId, function (err) {
-      //   if (err) throw err;
-      //   console.log('File deleted!');
-      // });
-
-
-      taskSchema.findOneAndDelete({topic:req.body.topic}).then(data=>{
-        scoreSchema.findOneAndDelete({taskId:data._id}).then(val=>{
-          let scoreArray = val.scores;
-          let ind;
-          scoreArray.map((val,index)=>{
-            if(val.rollNo===req.query.rollNo){
-              ind=index;
-            }
+      var form = new formidable.IncomingForm();
+      form.parse(req, function (err, fields, files){
+        taskSchema.findOneAndDelete({topic:fields.topic}).then(data=>{
+          scoreSchema.findOneAndDelete({taskId:data._id}).then(val=>{
+            let scoreArray = val.scores;
+            let ind;
+            scoreArray.map((val,index)=>{
+              if(val.rollNo===req.query.rollNo){
+                ind=index;
+              }
+            })
+            scoreArray.splice(ind,1);
+            new scoreSchema({_id:val._id,taskId:data._id,scores:scoreArray}).save();
           })
-          scoreArray.splice(ind,1);
-          new scoreSchema({_id:val._id,taskId:data._id,scores:scoreArray}).save();
-        })
-        let id = data._id;
-        let attachmentArray = data.attachments;
-        let ind;
-        let uploadedTime = data.uploadTime;
-        let setDeadline = data.deadline;
-        attachmentArray.map((data,index)=>{
-          if(data.rollNo===req.query.rollNo){
-            ind = index;
-          }
-        });
-        attachmentArray.splice(ind,1);
-        gfs.remove({_id:data.attachmentId,root:'uploads'});
-        new taskSchema({_id:data._id,taskType:req.body.taskType,topic:req.body.topic,attachments:attachmentArray,uploadTime:uploadedTime,deadline:setDeadline}).save().then(()=>res.status(200).send("Task submission cleared"))
-        .catch(err=>res.status(500).json(err));
+          let id = data._id;
+          let attachmentArray = data.attachments;
+          let ind;
+          let uploadedTime = data.uploadTime;
+          let setDeadline = data.deadline;
+          attachmentArray.map((data,index)=>{
+            if(data.rollNo===req.query.rollNo){
+              ind = index;
+              fs.unlink('./public'+data.attachmentId, function (err) {
+                if (err) throw err;
+                console.log('File deleted!');
+              });
+            }
+          });
+          attachmentArray.splice(ind,1);
+          new taskSchema({_id:data._id,taskType:fields.taskType,topic:fields.topic,attachments:attachmentArray,uploadTime:uploadedTime,deadline:setDeadline}).save().then(()=>res.status(200).send("Task submission cleared"))
+          .catch(err=>res.status(500).json(err));
+      })
     })
   }
 })
@@ -135,7 +108,10 @@ router.get('/removetask',(req,res)=>{
 
     let attachmentArray = data.attachments;
     attachmentArray.map(val=>{
-      gfs.remove({_id:val.attachmentId,root:'uploads'});
+      fs.unlink('./public'+val.attachmentId, function (err) {
+        if (err) throw err;
+        console.log('File deleted!');
+      });
     })
   }).then(()=>res.status(200).send("Task removed")).catch(err=>res.json(err))
 })
@@ -151,22 +127,7 @@ router.post('/modifytask',(req,res)=>{
   })
 })
 
-router.get('/attachment',(req,res)=>{
-  if(!req.query.topic || !req.query.rollNo){
-    res.status(400).send("Bad request");
-  }
-  taskSchema.findOne({topic:req.query.topic}).then(data=>{
-    data.attachments.map(obj=>{
-      if(obj.rollNo===req.query.rollNo){
-      gfs.files.findOne({ _id: obj.attachmentId }, (err, file) => {
-        const readStream = gfs.createReadStream(file.filename);
-        res.setHeader('Content-type','application/pdf');
-        readStream.pipe(res);
-    })
-    }
-  })})
 
-})
 
 router.post('/gradetask',(req,res)=>{
   if(!req.query.taskId){
