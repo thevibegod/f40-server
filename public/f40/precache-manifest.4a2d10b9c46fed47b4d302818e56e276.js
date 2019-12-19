c5dcfd3810ee8e5f5ded97c8475a078c3902177f
PGOKULNATH <4gokul2000@gmail.com>
2019-12-19 11:28:17 +0530

    student portal finished

---
+++ public/f40/precache-manifest.4a2d10b9c46fed47b4d302818e56e276.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8899ea2203ecf79a4d2dc5939d568e56",
+    "url": "/F40-erp/index.html"
+  },
+  {
+    "revision": "770a8dd872f3f280c747",
+    "url": "/F40-erp/static/css/2.fdaeb1af.chunk.css"
+  },
+  {
+    "revision": "770a8dd872f3f280c747",
+    "url": "/F40-erp/static/js/2.626e278a.chunk.js"
+  },
+  {
+    "revision": "9c24be26c26dfa583d39ced68f9b4e88",
+    "url": "/F40-erp/static/js/2.626e278a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a61e309e26964572f0a4",
+    "url": "/F40-erp/static/js/main.5d17cfa7.chunk.js"
+  },
+  {
+    "revision": "469c81bdbf9bb7be995f",
+    "url": "/F40-erp/static/js/runtime-main.fc97550f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/F40-erp/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/F40-erp/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/F40-erp/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/F40-erp/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/F40-erp/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
