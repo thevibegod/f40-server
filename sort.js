2c1532caf7ef26a912c7dd1ccfe5c1fdaea32921
Adarsh <adarsh@thevibegod.com>
2020-01-06 22:00:03 +0530

    Added Timestamp sorting functionality

---
+++ sort.js
@@ -0,0 +1,29 @@
+function convertDate(timestamp){
+  let dateObj = new Date();
+  dateObj.setDate(timestamp.slice(0,2));
+  dateObj.setMonth(parseInt(timestamp.slice(3,5))-1);
+  dateObj.setFullYear(timestamp.slice(6,10));
+  dateObj.setHours(timestamp.slice(11,13));
+  dateObj.setMinutes(timestamp.slice(14,16));
+  dateObj.setSeconds(timestamp.slice(17,19));
+  return dateObj;
+}
+
+function compare(a, b) {
+  const dateA = convertDate(a.timestamp);
+  const dateB = convertDate(b.timestamp);
+	const val = dateA - dateB;
+  let comparison = 0;
+  if (val > 0) {
+    comparison = 1;
+  } else if (val < 0) {
+    comparison = -1;
+  }
+  return comparison;
+}
+
+const sortArray =(arr)=>{
+  arr.sort(compare);
+}
+
+module.exports = sortArray;
